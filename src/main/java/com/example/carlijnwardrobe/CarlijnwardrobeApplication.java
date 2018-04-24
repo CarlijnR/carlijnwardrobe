35059e62b76af0aa67f414e75524e913077c6c60
CarlijnR <37950298+CarlijnR@users.noreply.github.com>
2018-04-24 11:51:54 +0200

    Initial commit

---
+++ src/main/java/com/example/carlijnwardrobe/CarlijnwardrobeApplication.java
@@ -0,0 +1,12 @@
+package com.example.carlijnwardrobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarlijnwardrobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarlijnwardrobeApplication.class, args);
+	}
+}
